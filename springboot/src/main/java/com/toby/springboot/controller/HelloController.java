3db0589522ae164046a48638b66bce95cf2e5f11
darkblose <darkblose@gmail.com>
2023-03-19 21:12:48 +0900

    Day03 : Standalone Spring Application

--- springboot/src/main/java/com/toby/springboot/controller/HelloController.java
+++ springboot/src/main/java/com/toby/springboot/controller/HelloController.java
@@ -1,29 +1,27 @@
 package com.toby.springboot.controller;
 
 import com.toby.springboot.service.HelloService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-@RequiredArgsConstructor
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
 
     private final HelloService helloService;
     private ApplicationContext applicationContext;
 
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+        this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println("applicationContext = " + applicationContext);
+    }
+
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("applicationContext = " + applicationContext);
     }
 }
