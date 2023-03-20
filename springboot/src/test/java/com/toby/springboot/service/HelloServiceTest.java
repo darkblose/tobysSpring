5bf8ba3b752d5f376b278e33488d4264b171a986
darkblose <darkblose@gmail.com>
2023-03-20 23:07:12 +0900

    Day04 : Decorator, Proxy Pattern

--- springboot/src/test/java/com/toby/springboot/service/HelloServiceTest.java
+++ springboot/src/test/java/com/toby/springboot/service/HelloServiceTest.java
@@ -1,5 +1,6 @@
 package com.toby.springboot.service;
 
+import com.toby.springboot.decorator.HelloDecorator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,5 +14,15 @@ class HelloServiceTest {
         String ret = helloService.sayHello("Test");
         // then
         assertThat(ret).isEqualTo("Hello Test");
+    }
+
+    @Test
+    public void helloDecorator() throws Exception {
+        // given
+        HelloDecorator helloDecorator = new HelloDecorator(name -> name);
+        // when
+        String ret = helloDecorator.sayHello("Test");
+        // then
+        assertThat(ret).isEqualTo("*Test*");
     }
 }
