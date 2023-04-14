6616637a3a0da35de62b360380fa75ba7ff8b272
darkblose <darkblose@gmail.com>
2023-04-14 23:26:33 +0900

    Day10 : Auto Configuration With External Configuration

--- springboot/src/main/java/com/toby/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/toby/springboot/SpringbootApplication.java
@@ -1,10 +1,22 @@
 package com.toby.springboot;
 
 import com.toby.config.annotation.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class SpringbootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("name = " + name);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
